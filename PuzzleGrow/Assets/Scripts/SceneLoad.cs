d4fa3b32fa1dfe6b612caaab102b44ce230dcd2a
yewon <wowuxo@gmail.com>
2021-01-25 19:17:42 +0900

    Basic
    기본틀잡음
    각 필요한 씬 생성
    로딩, 메인화면 이미지 추가

---
+++ PuzzleGrow/Assets/Scripts/SceneLoad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoad : MonoBehaviour
+{
+    public void MainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void StartScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void CollectionScene()
+    {
+        SceneManager.LoadScene("Collections");
+    }
+
+    public void RandomScene()
+    {
+        SceneManager.LoadScene("Random");
+    }
+
+    public void EventScene()
+    {
+        SceneManager.LoadScene("Event");
+    }
+}
